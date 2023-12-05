536d98e185deef3fd7f5d15aa05215e108e45375
zntt <zanattagg@gmail.com>
2023-12-05 20:53:07 -0300

    route

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,9 @@ import { Cookies } from "quasar";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://localhost:3000/api" });
+const api = axios.create({
+  baseURL: "http://sw3-prova-api-production.up.railway.app/api",
+});
 
 api.interceptors.request.use(
   function (config) {
